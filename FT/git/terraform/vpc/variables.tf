c6eba7aa650d60f14d5099c39206c65b0f522fa5
artewka <33059920+artewka@users.noreply.github.com>
2022-01-27 13:53:00 +0200

    Update variables.tf

--- FT/git/terraform/vpc/variables.tf
+++ FT/git/terraform/vpc/variables.tf
@@ -17,13 +17,13 @@ variable "public_cidr_block" {
     default = ["10.0.100.0/24", "10.0.200.0/24"]
 }
 
-variable "availability_zone" {
-    default = [
-        "eu-central-1a",
-        "eu-central-1b",
-        "eu-central-1c"
-    ] 
-}
+# variable "availability_zone" {
+#     default = [
+#         "eu-central-1a",
+#         "eu-central-1b",
+#         "eu-central-1c"
+#     ] 
+# }
 
 variable "private_cidr_block" {
     type    = list(string)
