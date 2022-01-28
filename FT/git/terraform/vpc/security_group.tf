8d6a1717ce76545a17d366b9513aaf47ebe17dbc
artewka <33059920+artewka@users.noreply.github.com>
2022-01-28 12:28:57 +0200

    Update security_group.tf

--- FT/git/terraform/vpc/security_group.tf
+++ FT/git/terraform/vpc/security_group.tf
@@ -54,31 +54,31 @@ dynamic "ingress" {
   }
 }
 
-resource "aws_security_group" "bastion" {
-  name        = "${var.environment}-allow_ssh"
-  description = "Allow ssh inbound traffic"
-  vpc_id      = aws_vpc.Vpc_Ter.id
+# resource "aws_security_group" "bastion" {
+#   name        = "${var.environment}-allow_ssh"
+#   description = "Allow ssh inbound traffic"
+#   vpc_id      = aws_vpc.Vpc_Ter.id
 
 
-dynamic "ingress" {
-    for_each = ["22"]
-    content {
-      from_port        = ingress.value
-      to_port          = ingress.value
-      protocol         = "tcp"
-      cidr_blocks      = [var.route_public_cidr_block]
-    }
-}
+# dynamic "ingress" {
+#     for_each = ["22"]
+#     content {
+#       from_port        = ingress.value
+#       to_port          = ingress.value
+#       protocol         = "tcp"
+#       cidr_blocks      = [var.route_public_cidr_block]
+#     }
+# }
 
-    egress {
-      from_port        = 0
-      to_port          = 0
-      protocol         = "-1"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-    }
+#     egress {
+#       from_port        = 0
+#       to_port          = 0
+#       protocol         = "-1"
+#       cidr_blocks      = ["0.0.0.0/0"]
+#       ipv6_cidr_blocks = ["::/0"]
+#     }
 
-  tags = {
-    Name = "allow ssh_bastion"
-  }
-}
+#   tags = {
+#     Name = "allow ssh_bastion"
+#   }
+# }
