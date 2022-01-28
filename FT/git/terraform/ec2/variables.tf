0ed2fb3ba466f752005924e3524baa174e1fffc0
artewka <33059920+artewka@users.noreply.github.com>
2022-01-28 13:30:24 +0200

    Update variables.tf

--- FT/git/terraform/ec2/variables.tf
+++ FT/git/terraform/ec2/variables.tf
@@ -1,25 +1,31 @@
+#default instance type
 variable "instance_type" {
     default     = "t2.micro"
     description = "ec2-instance type"
 }
 
+#default IAM role
 variable "iam_instance_profile" {
     default     = "EC2_SSM"
     description = "ec2-iam role"
 }
 
-
+#subnet id for ec2 instance
 variable "subnet_id" {
 }
 
+#security group for ec2 instance
 variable "security_group" {
 }
 
+#user data path for ec2 instance
 variable "user_data" {
 }
 
+#ssh-key for ec2 instance
 variable "key_name" {
 }
 
+#tag name for ec2 instance
 variable "srv" {
 }
