a72bd74fe7f2d3e01c210872f3918751ee6f1488
artewka <33059920+artewka@users.noreply.github.com>
2022-01-29 01:16:50 +0200

    Update bastion.sh

--- FT/git/terraform/bastion.sh
+++ FT/git/terraform/bastion.sh
@@ -2,4 +2,7 @@
 sudo apt update;
 sudo apt install software-properties-common;
 add-apt-repository --yes --update ppa:ansible/ansible;
-sudo apt install ansible;
+sudo apt install ansible -y;
+sudo apt-get install python3 -y;
+sudo apt-get install python3-pip -y;
+sudo pip3 install boto3;
