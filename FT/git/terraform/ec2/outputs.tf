65a218467dbf260fa092f6e2b80db9a2efae94fa
artewka <33059920+artewka@users.noreply.github.com>
2022-01-28 12:09:37 +0200

    Update outputs.tf

--- FT/git/terraform/ec2/outputs.tf
+++ FT/git/terraform/ec2/outputs.tf
@@ -1,6 +1,6 @@
-output "aws_instance_id" {
-  value = aws_instance.aws_instance[*].id
-}
+# output "aws_instance_id" {
+#   value = aws_instance.aws_instance[*].id
+# }
 
 output "public_ip" {
   value = aws_instance.aws_instance.public_ip
