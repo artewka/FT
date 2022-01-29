953dffce5e46ff7a31b8d9f1eabce5b0f1d9a29d
artewka <33059920+artewka@users.noreply.github.com>
2022-01-30 01:34:43 +0200

    Update security_group.tf

--- FT/DR/terraform/vpc/security_group.tf
+++ FT/DR/terraform/vpc/security_group.tf
@@ -1,17 +1,18 @@
+#security group for web-srv
 resource "aws_security_group" "web_srv" {
   name        = "${var.environment}-rules to open ports for web_srv"
   description = "Allow http inbound traffic"
   vpc_id      = aws_vpc.Vpc_Ter.id
 
   dynamic "ingress" {
-    for_each = ["80","22"]
+    for_each = ["22","80"]
     content {
       from_port        = ingress.value
       to_port          = ingress.value
       protocol         = "tcp"
-      cidr_blocks      = [var.public_cidr_block[0],var.public_cidr_block[1]]
-    }
+      cidr_blocks      = [var.public_cidr_block[0],var.public_cidr_block[1],var.bastion_ip[0]]
   }
+}  
     egress {
       from_port        = 0
       to_port          = 0
@@ -25,6 +26,7 @@ resource "aws_security_group" "web_srv" {
   }
 }
 
+#security group for db-srv
 resource "aws_security_group" "db_srv" {
   name        = "${var.environment}-allow_ssh,mysql"
   description = "Allow ssh,mysql inbound traffic"
@@ -37,7 +39,7 @@ dynamic "ingress" {
       from_port        = ingress.value
       to_port          = ingress.value
       protocol         = "tcp"
-      cidr_blocks      = [var.public_cidr_block[0],var.public_cidr_block[1]]
+      cidr_blocks      = [var.private_cidr_block[0],var.private_cidr_block[1],var.bastion_ip[0]]
     }
 }
 
@@ -51,34 +53,5 @@ dynamic "ingress" {
 
   tags = {
     Name = "allow mysql,http"
-  }
-}
-
-resource "aws_security_group" "bastion" {
-  name        = "${var.environment}-allow_ssh"
-  description = "Allow ssh inbound traffic"
-  vpc_id      = aws_vpc.Vpc_Ter.id
-
-
-dynamic "ingress" {
-    for_each = ["22"]
-    content {
-      from_port        = ingress.value
-      to_port          = ingress.value
-      protocol         = "tcp"
-      cidr_blocks      = [var.route_public_cidr_block]
-    }
-}
-
-    egress {
-      from_port        = 0
-      to_port          = 0
-      protocol         = "-1"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-    }
-
-  tags = {
-    Name = "allow ssh_bastion"
   }
 }
