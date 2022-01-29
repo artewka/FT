e675bca1007022b7695f9f4af414aeb28c996878
artewka <33059920+artewka@users.noreply.github.com>
2022-01-30 01:39:57 +0200

    Update backend.tf

--- FT/DR/terraform/backend.tf
+++ FT/DR/terraform/backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket     = "terraform-art-DR"
     key        = "terraform/backend/terraform.tfstate"
-    region     = var.region
+    region     = us-west1
   }
 }
