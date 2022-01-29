f3b61b326ec4ec84fa6a51e2de894968653e9704
artewka <33059920+artewka@users.noreply.github.com>
2022-01-29 02:50:47 +0200

    Update security_group.tf

--- FT/git/terraform/vpc/security_group.tf
+++ FT/git/terraform/vpc/security_group.tf
@@ -55,33 +55,3 @@ dynamic "ingress" {
     Name = "allow mysql,http"
   }
 }
-
-resource "aws_security_group" "bastion_srv" {
-  name        = "${var.environment}-allow_ssh"
-  description = "Allow ssh inbound traffic"
-  vpc_id      = aws_vpc.Vpc_Ter.id
-
-
-dynamic "ingress" {
-    for_each = ["22"]
-    content {
-      from_port        = ingress.value
-      to_port          = ingress.value
-      protocol         = "tcp"
-      cidr_blocks      = var.bastion_ip
-    }
-}
-
-    egress {
-      from_port        = 0
-      to_port          = 0
-      protocol         = "-1"
-      cidr_blocks      = ["0.0.0.0/0"]
-      ipv6_cidr_blocks = ["::/0"]
-    }
-
-  tags = {
-    Name = "allow ssh"
-  }
-}
-
