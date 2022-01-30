8b74f56824c8682689cd9295a65ef031c630a9fa
artewka <33059920+artewka@users.noreply.github.com>
2022-01-30 15:35:25 +0200

    Update variables.tf

--- FT/git/switch_to_blue/variables.tf
+++ FT/git/switch_to_blue/variables.tf
@@ -1,2 +1,30 @@
+#API key for Cloudflare
 variable "api_key" {
 }
+
+#S3 ucket name for remote state
+variable "bucket" {
+  default = "terraform-art"
+}
+
+#Path for tfstate
+variable "key" {
+  default = "terraform/backend/terraform.tfstate"
+}
+
+#Region for bucket
+variable "region" {
+  default = "eu-central-1"
+}
+
+#Zone id for domain
+variable "zone" {
+  default = "b1dd1fcec37ef9ccee3de24f8462ebb4"
+}
+
+#Domain name
+variable "name" {
+  default = "artewka.xyz"
+}
+
+
