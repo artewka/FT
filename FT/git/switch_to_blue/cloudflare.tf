fa946111c4e2d1fb9a268746188fd1cfba2c9141
artewka <33059920+artewka@users.noreply.github.com>
2022-01-30 15:29:05 +0200

    Update cloudflare.tf

--- FT/git/switch_to_blue/cloudflare.tf
+++ FT/git/switch_to_blue/cloudflare.tf
@@ -1,15 +1,15 @@
 data "terraform_remote_state" "remote" {
   backend = "s3"
   config = {
-      bucket     = "terraform-art"
-      key        = "terraform/backend/terraform.tfstate"
-      region     = "eu-central-1"
+      bucket     = var.bucket
+      key        = var.key
+      region     = var.region
    }
 }
 
 resource "cloudflare_record" "console-paas" {
-  zone_id         = "b1dd1fcec37ef9ccee3de24f8462ebb4"
-  name            = "artewka.xyz"
+  zone_id         = var.zone
+  name            = var.name
   value           = data.terraform_remote_state.remote.outputs.blue_alb_dns
   type            = "CNAME"
   proxied         = true
