7fb70877fb6139f3ce3406afe435a05825961737
artewka <33059920+artewka@users.noreply.github.com>
2022-01-31 00:35:11 +0200

    Update web_srv.tf

--- FT/DR/terraform/web_srv.tf
+++ FT/DR/terraform/web_srv.tf
@@ -1,14 +1,15 @@
-
+#Network module
 module "vpc" {
   source = "./vpc"
 }
 
+#Creating instances with tags (srv variable)
  module "web" {
    source = "./ec2"
 
     count                        = 2
-    subnet_id                    = element(module.vpc.public_subnet_ids[*],count.index)
-    security_group               = [module.vpc.sg_public]
+    subnet_id                    = element(module.vpc.private_subnet_ids[*],count.index)
+    security_group               = [module.vpc.sg_web]
     user_data                    = false
     key_name                     = aws_key_pair.ssh_key.key_name
     srv                          = "web"
@@ -18,8 +19,8 @@ module "vpc" {
    source = "./ec2"
 
     count                        = 2
-    subnet_id                    = element(module.vpc.public_subnet_ids[*],count.index)
-    security_group               = [module.vpc.sg_public]
+    subnet_id                    = element(module.vpc.private_subnet_ids[*],count.index)
+    security_group               = [module.vpc.sg_web]
     user_data                    = false
     key_name                     = aws_key_pair.ssh_key.key_name
     srv                          = "phpmyadmin"
@@ -30,7 +31,7 @@ module "vpc" {
    source = "./ec2"
    
     subnet_id                    = module.vpc.db_subnet_ids
-    security_group               = [module.vpc.sg_private]
+    security_group               = [module.vpc.sg_db]
     user_data                    = false
     key_name                     = aws_key_pair.ssh_key.key_name
     srv                          = "db"
