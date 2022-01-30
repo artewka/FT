7632a0079eb1ce18878805b453310601a6f8c0a9
artewka <33059920+artewka@users.noreply.github.com>
2022-01-31 00:50:20 +0200

    Update outputs.tf

--- FT/DR/terraform/vpc/outputs.tf
+++ FT/DR/terraform/vpc/outputs.tf
@@ -1,26 +1,32 @@
+#Public subnet ids
 output "public_subnet_ids" {
   description = "subnet ID"
   value       = aws_subnet.Public[*].id
 }
 
+#Private subnet ids
 output "private_subnet_ids" {
   description = "subnet ID"
   value       = aws_subnet.Private[*].id
 }
 
+#DB subnet ids
 output "db_subnet_ids" {
   description = "subnet ID"
   value       = aws_subnet.DB.id
 }
 
+#VPC id
 output "vpc_cidr_id" {
   value       = aws_vpc.Vpc_Ter.id
 }
 
-output "sg_public" {
+#Security group id for public subnet
+output "sg_web" {
   value = aws_security_group.web_srv.id
 }
 
-output "sg_private" {
+#Security group id for private subnet
+output "sg_db" {
   value = aws_security_group.db_srv.id
 }
